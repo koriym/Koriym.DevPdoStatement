c33fae7b5562c4f5f8c9e1f202ed9ad0ab80c77c
Akihito Koriyama <akihito.koriyama@gmail.com>
2016-02-24 17:04:43 +0900

    full table scan test

--- tests/DevPdoStatementTest.php
+++ tests/DevPdoStatementTest.php
@@ -22,39 +22,50 @@ class DevPdoStatementTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         parent::setUp();
-        $this->pdo = new \PDO('mysql:host=localhost', 'root');
+        $this->pdo = new \PDO('mysql:host=localhost;', 'root');
         $this->logger = new Logger;
-        $this->pdo->setAttribute(\PDO::ATTR_STATEMENT_CLASS, [DevPdoStatement::class, [$this->pdo, $this->logger]]);
-        $this->pdo->exec('DROP DATABASE IF EXISTS tmp;');
-        $this->pdo->exec('CREATE DATABASE IF NOT EXISTS tmp; use tmp;');
-        $this->pdo->exec('CREATE TABLE IF NOT EXISTS user(id integer primary key, name text)');
+        $this->pdo->exec('CREATE DATABASE IF NOT EXISTS tmp;');
+        $this->pdo->exec('USE tmp;');
+        $this->pdo->exec('CREATE TABLE user(id integer, name text)');
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->pdo->setAttribute(\PDO::ATTR_STATEMENT_CLASS, [DevPdoStatement::class, [$this->pdo, $this->logger]]);
     }
 
-    public function testNewInstance()
+    protected function tearDown()
     {
-        $this->sth = $this->pdo->prepare('insert into user(name) values (:name)');
-        $this->assertInstanceOf('\Koriym\DevPdoStatement\DevPdoStatement', $this->sth);
-
-        return $this->sth;
+        $this->pdo->exec('DROP DATABASE tmp;');
     }
 
-    /**
-     * @depends testNewInstance
-     */
-    public function testInterpolateQuery(DevPdoStatement $sth)
+    public function testInterpolateQuery()
     {
+        $this->pdo->setAttribute(\PDO::ATTR_STATEMENT_CLASS, [DevPdoStatement::class, [$this->pdo, $this->logger]]);
+        $sth = $this->pdo->prepare('INSERT INTO user(id, name) VALUES (:id, :name)');
+        $this->assertInstanceOf('\Koriym\DevPdoStatement\DevPdoStatement', $sth);
+    }
+
+    public function testBindValue()
+    {
+        $sth = $this->pdo->prepare('INSERT INTO user(id, name) VALUES (:id, :name)');
+        $sth->bindValue(':id', 1, \PDO::PARAM_STR);
         $sth->bindValue(':name', 'koriym', \PDO::PARAM_STR);
         $sth->execute();
-        $this->assertSame("insert into user(name) values ('koriym')", $sth->interpolateQuery);
+        $this->assertSame("INSERT INTO user(id, name) VALUES (1, 'koriym')", $sth->interpolateQuery);
     }
 
-    public function testExplain()
+    public function testExpalin()
     {
-        $this->sth = $this->pdo->prepare('select name from user where id = :id');
-        $this->sth->bindValue(':id', 1, \PDO::PARAM_INT);
+        $sth = $this->pdo->prepare('INSERT INTO user(id, name) VALUES (:id, :name)');
+        $sth->bindParam(':id', $id, \PDO::PARAM_STR);
+        $sth->bindParam(':name', $name, \PDO::PARAM_STR);
+        for ($i = 0; $i < 100; $i++) {
+            $id = $i;
+            $name = 'koriym' . (string)$i;
+            $sth->execute();
+        }
+        $this->sth = $this->pdo->prepare('select id, name from user where id > :id');
+        $this->sth->bindValue(':id', 80, \PDO::PARAM_INT);
         $this->sth->execute();
-        $this->sth->fetchAll();
+        $this->sth->fetchAll(\PDO::FETCH_ASSOC);
         $this->logger;
         $expected = 'SIMPLE';
         $this->assertSame($this->logger->explain[0]['select_type'], $expected);
